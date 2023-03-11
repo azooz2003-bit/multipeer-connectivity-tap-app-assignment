8215612c19ba75a707e75100030d4f3fa0d77887
Abdulaziz Albahar <67667005+azooz2003-bit@users.noreply.github.com>
2023-03-11 16:37:05 -0500

    Initial Commit

---
+++ GUESS-E_MiniTapApp/GUESS_E_MiniTapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GUESS_E_MiniTapAppApp.swift
+//  GUESS-E_MiniTapApp
+//
+//  Created by Abdulaziz Albahar on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct GUESS_E_MiniTapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
